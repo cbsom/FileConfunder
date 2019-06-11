9c90294d09c2699a624ed39643bebe2218e0ca7f
Chaim B. Sommers <cb@compute.co.il>
2019-06-11 16:26:24 +0300

    Made path a proper argument to allow it to be anywhere in the arg list. Allowed silent run.

--- FileConfunder/Program.cs
+++ FileConfunder/Program.cs
@@ -7,12 +7,16 @@ namespace FileConfunder
 {
     class Program
     {
+        private static string _path = null;
+        private static string _action = "run";
+        private static string _pattern = "*";
         private static int _bufferLength = Properties.Settings.Default.BufferSize;
         private static int _offset = Properties.Settings.Default.ByteOffset;
         private static string _runWith = Properties.Settings.Default.OpenWithPath;
-        private const string HELP_TEXT = @"USAGE: fc fileOrFolderPath [-action action]
+        private static bool _silent;
+        private const string HELP_TEXT = @"USAGE: fnc -path fileOrFolderPath -action actionName
     If ""-action"" is not suplied, the default action is ""run"".
-    The optional ""action"" argument value can be one of the following:
+    The ""-action"" argument value can be any one of the following values:
         ""run"" unconfund then run. When app exits, reconfunds the file.
         ""confund"" confund the given file 
         ""unconfund"" unconfund the given file 
@@ -24,22 +28,21 @@ namespace FileConfunder
         [-runwith *] for use with the action ""run"". If  supplied, after unconfundation, the file will be run with the given path. Otherwise the path set in the config file is used.
         [-pattern *] for use with the action ""confundall"" or ""unconfundall"". If supplied, only those files matching the pattern will be processed.
         [-buflen *] If supplied, the given number of bytes in the file will be ""confunded"". Otherwise the value set in the config file is used. NOTE: files need to be unconfunded with the same buffer length and offset values whith which they were originally confunded.
-        [-offset *] If supplied, during ""confunding"" or ""unconfunding"" process, each byte will be offset by the given number. Otherwise the value set in the config file is used. NOTE: files need to be unconfunded with the same buffer length and offset values whith which they were originally confunded.";
-
+        [-offset *] If supplied, during ""confunding"" or ""unconfunding"" process, each byte will be offset by the given number. Otherwise the value set in the config file is used. NOTE: files need to be unconfunded with the same buffer length and offset values whith which they were originally confunded.
+        [-silent] If supplied, the command window is hidden and automatically. Otherwise, the user needs to press enter to exit.";
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args.Length < 2 || !args.Contains("-path"))
             {
                 Console.WriteLine(HELP_TEXT);
                 return;
             }
-            string path = args[0],
-                   action = "run",
-                   pattern = "*";
+
+            _path = args[Array.IndexOf(args, "-path") + 1];
 
             if (args.Contains("-action"))
             {
-                action = args[Array.IndexOf(args, "-action") + 1];
+                _action = args[Array.IndexOf(args, "-action") + 1];
             }
             if (args.Contains("-runwith"))
             {
@@ -47,7 +50,7 @@ namespace FileConfunder
             }
             if (args.Contains("-pattern"))
             {
-                pattern = args[Array.IndexOf(args, "-pattern") + 1];
+                _pattern = args[Array.IndexOf(args, "-pattern") + 1];
             }
             if (args.Contains("-buflen"))
             {
@@ -59,7 +62,7 @@ namespace FileConfunder
                 }
                 else
                 {
-                    Console.WriteLine("{0} is not a valid value for the -buflen argument", str);
+                    SpitOut("{0} is not a valid value for the -buflen argument", str);
                     return;
                 }
             }
@@ -73,81 +76,104 @@ namespace FileConfunder
                 }
                 else
                 {
-                    Console.WriteLine("{0} is not a valid value for the -offset argument", str);
+                    SpitOut("{0} is not a valid value for the -offset argument", str);
                     return;
                 }
             }
-            if (action != "help" && ((action.EndsWith("all") && !Directory.Exists(path)) ||
-                (!action.EndsWith("all") && !File.Exists(path))))
+            if (args.Contains("-silent"))
             {
-                Console.WriteLine("{0} cannot be found", path);
+                _silent = true;
+                try
+                {
+                    Console.SetBufferSize(1, 1);
+                    Console.SetWindowSize(1, 1);
+                    Console.SetWindowPosition(-1, -1);
+                }
+                catch { }
+            }
+            if (_action != "help" && ((_action.EndsWith("all") && !Directory.Exists(_path)) ||
+                (!_action.EndsWith("all") && !File.Exists(_path))))
+            {
+                SpitOut("{0} cannot be found", _path);
                 return;
             }
 
-            switch (action)
+            switch (_action)
             {
                 case "confundall":
-                    foreach (var f in Directory.GetFiles(path, pattern))
+                    foreach (var f in Directory.GetFiles(_path, _pattern))
                     {
-                        Console.WriteLine("{0} has {1} confunded", f,
+                        SpitOut("{0} has {1} confunded", f,
                             ConfundFile(f)
                                 ? "been successfully"
                                 : "FAILED to be");
                     }
-                    Console.WriteLine("{0} has been processed", path);
+                    SpitOut("{0} has been processed", _path);
                     break;
                 case "unconfundall":
-                    foreach (var f in Directory.GetFiles(path, pattern))
+                    foreach (var f in Directory.GetFiles(_path, _pattern))
                     {
-                        Console.WriteLine("{0} has {1} unconfunded", f,
+                        SpitOut("{0} has {1} unconfunded", f,
                             ConfundFile(f, true)
                                 ? "been successfully"
                                 : "FAILED to be");
                     }
-                    Console.WriteLine("{0} has been processed", path);
+                    SpitOut("{0} has been processed", _path);
                     break;
                 case "confund":
-                    Console.WriteLine("{0} has {1} confunded", path,
-                        ConfundFile(path)
+                    SpitOut("{0} has {1} confunded", _path,
+                        ConfundFile(_path)
                             ? "been successfully"
                             : "FAILED to be");
                     break;
                 case "unconfund":
-                    Console.WriteLine("{0} has {1} unconfunded", path,
-                        ConfundFile(path, true)
+                    SpitOut("{0} has {1} unconfunded", _path,
+                        ConfundFile(_path, true)
                             ? "been successfully"
                             : "FAILED to be");
                     break;
                 case "run":
                 case "rununconfund":
-                    if (ConfundFile(path, true))
+                    if (ConfundFile(_path, true))
                     {
-                        Console.WriteLine("{0} has been successfully unconfunded", path);
-                        Console.WriteLine("Running {0} {1}", _runWith, path);
-                        using (var pr = Process.Start(_runWith, path))
+                        SpitOut("{0} has been successfully unconfunded", _path);
+                        SpitOut("Running {0} {1}", _runWith, _path);
+                        using (var pr = Process.Start(_runWith, _path))
                         {
-                            if (action == "run")
+                            if (_action == "run")
                             {
                                 pr.WaitForExit();
-                                Console.WriteLine("{0} has exited", Path.GetFileName(_runWith));
-                                Console.WriteLine("Starting to reconfund {0}", path);
-                                Console.WriteLine("{0} has {1} re-confunded", path,
-                                    ConfundFile(path) ? "been successfully" : "FAILED to be");
+                                SpitOut("{0} has exited", Path.GetFileName(_runWith));
+                                SpitOut("Starting to reconfund {0}", _path);
+                                SpitOut("{0} has {1} re-confunded", _path,
+                                    ConfundFile(_path) ? "been successfully" : "FAILED to be");
                             }
                         }
                     }
                     else
                     {
-                        Console.WriteLine("{0} cannot be unconfunded", path);
+                        SpitOut("{0} cannot be unconfunded", _path);
                     }
                     break;
                 default:
-                    Console.WriteLine(HELP_TEXT);
+                    SpitOut(HELP_TEXT);
                     break;
             }
 
-            Console.WriteLine("...........................\n\nPress <ENTER> to exit...");
+            if (_silent)
+            {
+                return;
+            }
+            SpitOut("...........................\n\nPress <ENTER> to exit...");
             Console.ReadLine();
+        }
+
+        private static void SpitOut(string txt, params object[] par)
+        {
+            if (!_silent)
+            {
+                Console.WriteLine(txt, par);
+            }
         }
 
         private static bool ConfundFile(string path, bool unconfund = false)
